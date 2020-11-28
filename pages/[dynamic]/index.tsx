4a1b048ebc4f36459608db7fa006acf5bcb2621b
JunIshino <jun.ishino@kepple.jp>
2020-11-28 22:35:59 +0900

    OGPの画像を生成するように修正

--- pages/[dynamic]/index.tsx
+++ pages/[dynamic]/index.tsx
@@ -1,13 +1,27 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import React from 'react'
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import fetchWrapper from '../../utils/FetchUtils';
 
 const DynamicPage: NextPage = () => {
   const router = useRouter();
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? '';
+  const { dynamic } = router.query; 
+
   return (
-    <div>
-      <h1>{router.query.dynamic}のページだよ</h1>
-    </div>
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" key="ogImage" content={`${baseUrl}/ogp/${dynamic}.png`} />
+        <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
+        <meta name="twitter:image" key="twitterImage" content={`${baseUrl}/ogp/${dynamic}.png`} />
+      </Head>
+      <div>
+        <h1>{dynamic}のページだよ</h1>
+      </div>
+    </>
   )
 }
 
@@ -22,6 +36,10 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
+  [...Array(10)].forEach((_, index) => {
+    void fetchWrapper.post(`/api/${index}/ogp`);
+  })
+
   return {
     props: {},
   }
---
+++ utils/FetchUtils.ts
@@ -0,0 +1,60 @@
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? '';
+
+const handleResponse = <T>(res: Response) => {
+  return res.text().then((text) => {
+    const data: T = JSON.parse(text) as T;
+
+    if (!res.ok) {
+      const error = `${res.status} ${res.statusText}`;
+
+      return Promise.reject(error);
+    }
+
+    return data;
+  });
+};
+
+const get = <T>(url: string): Promise<T> => {
+  const requestOptions = {
+    method: 'GET',
+  };
+
+  return fetch(baseUrl + url, requestOptions).then<T>(handleResponse);
+};
+
+const post = <T>(url: string, body?: object): Promise<T> => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  };
+
+  return fetch(baseUrl + url, requestOptions).then<T>(handleResponse);
+};
+
+const put = <T>(url: string, body: object): Promise<T> => {
+  const requestOptions = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body),
+  };
+
+  return fetch(baseUrl + url, requestOptions).then<T>(handleResponse);
+};
+
+const deleteApi = <T>(url: string): Promise<T> => {
+  const requestOptions = {
+    method: 'DELETE',
+  };
+
+  return fetch(baseUrl + url, requestOptions).then<T>(handleResponse);
+};
+
+const fetchWrapper = {
+  get,
+  post,
+  put,
+  delete: deleteApi,
+};
+
+export default fetchWrapper;

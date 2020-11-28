df77e395486bae317078f70d09058f5a556b5706
JunIshino <jun.ishino@kepple.jp>
2020-11-28 23:56:25 +0900

    本番環境でもcanvasが動くように修正

--- pages/[dynamic]/index.tsx
+++ pages/[dynamic]/index.tsx
@@ -16,6 +16,8 @@ const DynamicPage: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:image" key="ogImage" content={`${baseUrl}/ogp/${dynamic}.png`} />
         <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
+        <meta name="twitter:title" content={`これはテストだよ`} />
+        <meta name="twitter:description" content={"これはテストだよ"} />
         <meta name="twitter:image" key="twitterImage" content={`${baseUrl}/ogp/${dynamic}.png`} />
       </Head>
       <div>
